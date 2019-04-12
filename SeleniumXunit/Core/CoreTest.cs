827cd9198b75827a42fa87a3dc47d57ba09d08e4
evionadc <michell.soares@outlook.com>
2019-04-12 16:16:43 -0300

    FirstCommit

---
+++ SeleniumXunit/Core/CoreTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace SeleniumXunit.Core
+
+{
+    public class CoreTest : IDisposable
+    {
+
+        public CoreTest() {
+
+            DriverFactory.GetDriver().Url = "http://www.google.com.br";
+
+        }
+
+
+       
+
+        public void Dispose()
+        {
+            DriverFactory.KillDriver();
+        }
+
+    }
+}
---
+++ SeleniumXunit/Core/DriverFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+
+namespace SeleniumXunit.Core
+{
+    public class DriverFactory
+    {
+        private static IWebDriver _driver;
+
+        private DriverFactory(){}
+
+        public static IWebDriver GetDriver() {
+
+            if (_driver == null)
+            {
+                switch (Property._browser)
+                {
+                    case Property.Browsers.Chrome:
+                        _driver = new ChromeDriver("/Users/e-msoares/Projects/SeleniumDotNet/SeleniumXunit/Driver");
+                        break;
+                    case Property.Browsers.Firefox:
+                        _driver = new FirefoxDriver();
+                        break;
+                }
+            }
+
+            return _driver;
+        }
+
+        public static void KillDriver() { 
+        
+            if(_driver != null)
+            {
+                _driver.Quit();
+                _driver = null;
+            }
+        
+        }
+    }
+}
